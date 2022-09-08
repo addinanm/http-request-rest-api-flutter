4f59d965fafa42b019d1a2027a4f270ded4f29dc
addina nurul <addinanurulmawaddah39@gmail.com>
2022-09-08 11:34:30 +0700

    list pupuler movie

--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -1,19 +1,23 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:http_request_rest_api_flutter/models/movie.dart';
 
 class HttpService {
   final String apiKey = '465c96dd1cf209e6dfdb30b1c3e4e6e8';
   final String baseUrl =
       'https://api.themoviedb.org/3/movie/popular?api_key=465c96dd1cf209e6dfdb30b1c3e4e6e8&language=en-US&page=1';
 
-  Future<String?> getPopularMovies() async {
+  Future<List?> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("Sukses");
-      String response = result.body;
-      return response;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
       return null;
